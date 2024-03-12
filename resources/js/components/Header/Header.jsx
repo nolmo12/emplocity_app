7b5314dab080b84b8f53a498e8fdef6c773d11b8
barpurzycki <bartekpurzycki01@gmail.com>
2024-03-12 19:36:15 +0100

    [FE][5]Home Page CSS Poprawki
    Poprawilem CSS strony głównej

--- resources/js/components/Header/Header.jsx
+++ resources/js/components/Header/Header.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import tempLogo from "./tempLogo.png";
 import { Link } from "react-router-dom";
 import tempIcon from "./ico.png";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./header.module.css";
 import AuthUser from "../AuthUser";
+
 export default function Header() {
     const { getToken, logout } = AuthUser();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <>
             <header>
                 <img src={tempLogo} alt="Logo" id={styles.imgLogo}></img>
                 <SearchBar />
-                <img src={tempIcon} alt="Icon" id={styles.imgIcon}></img>
+                <img
+                    src={tempIcon}
+                    alt="Icon"
+                    id={styles.imgIcon}
+                    onClick={toggleMenu}
+                ></img>
             </header>
-            <Link to="/register">
-                <button id={styles.register}>Register</button>
-            </Link>
-
-            {!getToken() && (
-                <Link to="/login">
-                    <button id={styles.login}>Login</button>
-                </Link>
+            {showMenu && (
+                <ul id={styles.menu}>
+                    <li>
+                        <Link to="/register">
+                            <button id={styles.register}>Register</button>
+                        </Link>
+                    </li>
+                    {!getToken() && (
+                        <li>
+                            <Link to="/login">
+                                <button id={styles.login}>Login</button>
+                            </Link>
+                        </li>
+                    )}
+                    <li>
+                        <button onClick={logout} id={styles.logout}>Logout</button>
+                    </li>
+                </ul>
             )}
-            <button id={styles.logout} onClick={logout}>
-                Logout
-            </button>
         </>
     );
 }
