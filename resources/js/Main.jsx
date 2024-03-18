b8791ebfb4d51b4661ddf633512b88b9c685ade6
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-03-18 13:46:20 +0100

    [FE] Import priority fix

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
-import TempLogin from "./components/TempLogin/TempLogin";
 import authUser from "./components/authUser";
+
 function Main() {
     const { getToken, isLogged } = authUser();
     return (
@@ -31,7 +31,6 @@ function Main() {
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgotPassword" element={<ForgotPassword />}></Route>
-            <Route path="/logged" element={<TempLogin />}></Route>
         </Routes>
     );
 }
