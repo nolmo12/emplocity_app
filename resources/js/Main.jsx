9ba849789a576478fa50417ac1ffa8a4238878a5
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-03-26 18:51:49 +0100

    [FE] remove useless

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ForgotPasswordPage from "./components/ForgotPasswordPage/ForgotPasswordPage";
+import RecoveryPasswordPage from "./components/RecoveryPasswordPage/RecoveryPasswordPage";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
@@ -11,7 +12,7 @@ import authUser from "./components/authUser";
 function Main() {
     const { getToken, isLogged } = authUser();
 
-    
+    // http://127.0.0.1/api/reset-password?token=5e58529ad4cc026665d286c5f4f6491722bff295ccaa532b8b94c2bd9237a1f0
     return (
         <Routes>
             <Route
@@ -34,6 +35,7 @@ function Main() {
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/forgotPassword" element={<ForgotPasswordPage componentType={false}/>}></Route>
+            <Route path="/reset-password/:token" element={<ResetPasswordPage />}></Route>
             <Route path="/upload" element={<UploadPage />}></Route>
             
         </Routes>
--- resources/js/components/RecoveryPasswordPage/RecoveryPasswordPage.jsx
+++ resources/js/components/RecoveryPasswordPage/RecoveryPasswordPage.jsx
@@ -1,3 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import authUser from "../authUser";
+
 export default function RecoveryPasswordPage(){
+    const { http, getCsrfToken } = authUser();
+    const [data, setData] = useState({
+        password: "",
+        repeatPassword: "",
+    });
+    function handleInputPassword(e){
+        setData({...data, password: e.target.value})
+    }
+
+    function handleInputRepeatPassword(e){
+        setData({...data, repeatPassword: e.target.value})
+    }
+
+    const handleSubmit = () => {
+        try{
+            http.post("/api/reset-password", {
+                token: getCsrfToken(), //fsdfsdfsdfsdfsd
+                password: data.password,
+            })
+        }catch(error){
+            console.log(error)
+        }
+
+    }
+    return (
+        <main>
+            <form onSubmit={handleSubmit}>
+            <input
+                    id={styles.Password}
+                    type="password"
+                    placeholder="Password"
+                    value={data.password}
+                    onChange={(e) => handleInputPassword(e)}
+                ></input>
+                <input
+                    id={styles.Password}
+                    type="password"
+                    placeholder="Repeat password"
+                    value={data.repeatPassword}
+                    onChange={(e) => handleInputRepeatPassword(e)}
+                ></input>
+                <button>Reset</button>
+            </form>
+        </main>
+    )
     
 }
