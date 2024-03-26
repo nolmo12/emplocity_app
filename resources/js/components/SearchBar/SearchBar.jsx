d4cb4670257400769ff222eb47978e4a8a40ed4b
barpurzycki <bartekpurzycki01@gmail.com>
2024-03-26 20:47:35 +0100

    Home Page CSS changes
    Home Page CSS changes, added elements to menu, added icons

--- resources/js/components/SearchBar/SearchBar.jsx
+++ resources/js/components/SearchBar/SearchBar.jsx
@@ -2,31 +2,15 @@ import React from "react";
 import { useState, useEffect } from "react";
 import fetchImage from "../fetchImgFromStorage";
 import styles from "./searchBar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function SearchBar() {
-    const [loopPath, setLoopPath] = useState("");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const loopPath = await fetchImage("loop.png");
-                setLoopPath(loopPath);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchData();
-    }, []);
     return (
         <div id={styles.searchBarLoop}>
             <input type="text" id={styles.searchBar}></input>
-            {loopPath && (
-                <img
-                    src={loopPath}
-                    alt="SearchLoop"
-                    id={styles.searchLoop}
-                ></img>
-            )}
+            <FontAwesomeIcon icon={faSearch} id={styles.searchLoop}/>
         </div>
     );
 }
