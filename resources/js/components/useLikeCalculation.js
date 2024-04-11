51e34d123c2168c00da5aab137a55669af5e9ffc
dzw <100282489+PiotrMarcinczuk@users.noreply.github.com>
2024-04-11 11:52:23 +0200

    [FE][12] Video frame

--- resources/js/components/useLikeCalculation.js
+++ resources/js/components/useLikeCalculation.js
@@ -5,7 +5,7 @@ export default function useLikeCalculation() {
     // po ponownym odpaleniu filmiku nie mam mozliwosci zmiany co dalem + jesli zostawilem na np 1 to dzialam z 1 do 2
     const [userThumb, setUserThumb] = useState("");
     const { sendLike } = useSendLikeInfo();
-    const likeCountFunction = (
+    const likeCountFunction = async (
         likeType,
         likesCount,
         setLikesCount,
@@ -18,7 +18,7 @@ export default function useLikeCalculation() {
         if (likeType === 1 && userThumb !== "like" && userThumb !== "dislike") {
             setLikesCount((prev) => prev + 1);
             setUserThumb("like");
-            sendLike(reference_code, likeType);
+            await sendLike(reference_code, likeType);
         } else if (
             likeType === 0 &&
             userThumb !== "dislike" &&
@@ -26,11 +26,11 @@ export default function useLikeCalculation() {
         ) {
             setDislikesCount((prev) => prev + 1);
             setUserThumb("dislike");
-            sendLike(reference_code, likeType);
+            await sendLike(reference_code, likeType);
         } else if (likeType === 1 && userThumb === "like" && likesCount > 0) {
             setLikesCount((prev) => prev - 1);
             setUserThumb("");
-            sendLike(reference_code, likeType);
+            await sendLike(reference_code, likeType);
         } else if (
             likeType === 0 &&
             userThumb === "dislike" &&
@@ -38,17 +38,17 @@ export default function useLikeCalculation() {
         ) {
             setDislikesCount((prev) => prev - 1);
             setUserThumb("");
-            sendLike(reference_code, likeType);
+            await sendLike(reference_code, likeType);
         } else if (likeType === 0 && userThumb === "like") {
             setLikesCount((prev) => prev - 1);
             setDislikesCount((prev) => prev + 1);
             setUserThumb("dislike");
-            sendLike(reference_code, likeType);
+            await sendLike(reference_code, likeType);
         } else if (likeType === 1 && userThumb === "dislike") {
             setLikesCount((prev) => prev + 1);
             setDislikesCount((prev) => prev - 1);
             setUserThumb("like");
-            sendLike(reference_code, likeType);
+            await sendLike(reference_code, likeType);
         }
         console.log(videoObj);
     };
