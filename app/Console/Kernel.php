b7eab31790940283e4de07060c10c57f0b06a1ca
Paweł Bąk <92019664+nolmo12@users.noreply.github.com>
2024-06-02 21:56:09 +0200

    [BE][8] Video CRUD

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,9 +10,11 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:clean-old-videos')->daily();
+        $schedule->command('app:clean-old-ips')->daily();
+        $schedule->command('app:clear-url')->daily();
     }
 
     /**
