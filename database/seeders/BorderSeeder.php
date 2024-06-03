736ab89d61b86f6df5b447c40a2b944d78930e19
Paweł Bąk <92019664+nolmo12@users.noreply.github.com>
2024-06-03 22:01:51 +0200

    [BE][18] Shop CRUD

--- database/seeders/BorderSeeder.php
+++ database/seeders/BorderSeeder.php
@@ -14,59 +14,40 @@ class BorderSeeder extends Seeder
     public function run(): void
     {
         Border::create([
-            'rarity' => 1,
-            'type' => '/storage/borders/Amethyst.png',
-            'price' => 10.0
-        ]);
-        Border::create([
-            'rarity' => 1,
-            'type' => '/storage/borders/Jade.png',
-            'price' => 10.0
-        ]);
-        Border::create([
-            'rarity' => 1,
-            'type' => '/storage/borders/Ruby.png',
-            'price' => 10.0
-        ]);
-        Border::create([
-            'rarity' => 1,
-            'type' => '/storage/borders/Sapphire.png',
-            'price' => 10.0
-        ]);
-        Border::create([
-            'rarity' => 1,
-            'type' => '/storage/borders/Topaz.png',
-            'price' => 10.0
-        ]);
-        Border::create([
             'rarity' => 2,
             'type' => '/storage/borders/Topaz_ring.png',
-            'price' => 15.0
+            'price' => 15.0,
+            'name' => 'Topaz Ring'
         ]);
         Border::create([
             'rarity' => 2,
             'type' => '/storage/borders/Angry.png',
-            'price' => 15.0
+            'price' => 15.0,
+            'name' => 'Angery!!!'
         ]);
         Border::create([
             'rarity' => 3,
             'type' => '/storage/borders/Bubble.png',
-            'price' => 20.0
+            'price' => 20.0,
+            'name' => 'Bubble'
         ]);
         Border::create([
             'rarity' => 4,
             'type' => '/storage/borders/Cat_ears.png',
-            'price' => 25.0
+            'price' => 25.0,
+            'name' => 'UwU'
         ]);
         Border::create([
             'rarity' => 4,
             'type' => '/storage/borders/Dragons.png',
-            'price' => 25.0
+            'price' => 25.0,
+            'name' => 'Dragons',
         ]);
         Border::create([
             'rarity' => 4,
             'type' => '/storage/borders/Neon.png',
-            'price' => 25.0
+            'price' => 25.0,
+            'name' => 'Neon'
         ]);
     }
 }
